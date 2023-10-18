d14ee46b3b08c1d051d2cfa45596dc30befce221
Denis Costello <G00396309@atu.ie>
2023-10-18 09:41:13 +0100

    Added rest template constructor

--- src/main/java/ie/atu/week5blockingapplication/BlockingService.java
+++ src/main/java/ie/atu/week5blockingapplication/BlockingService.java
@@ -1,4 +1,11 @@
 package ie.atu.week5blockingapplication;
 
+import org.springframework.web.client.RestTemplate;
+
 public class BlockingService {
+    private final RestTemplate restTemplate;
+
+    public BlockingService(RestTemplate restTemplate){
+        this.restTemplate=restTemplate;
+    }
 }
